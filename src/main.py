1f3a45f4c0c8b527bef617c0926aa86013a34a0b
Kafka2306 <kafka@example.com>
2026-02-11 22:12:25 +0900

    chore: global sync

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import torch
 from diffusers import ZImagePipeline
-
 pipe = ZImagePipeline.from_pretrained(
     "Tongyi-MAI/Z-Image-Turbo", torch_dtype=torch.bfloat16, low_cpu_mem_usage=False
 ).to("cuda")
